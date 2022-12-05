b41900f1df43f2d2aef11a42861a2566fdae8e24
Luiz Sementille <luigifernando2011@gmail.com>
2022-12-05 11:24:41 -0300

    Removing unecessary spaces.

--- recipes/views.py
+++ recipes/views.py
@@ -3,8 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-#Usando o argumento context, é possível enviar variáveis para a template especificada.
-
+# Usando o argumento context, é possível enviar variáveis para a template especificada.
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name':'Luiz Fernando Sementille',
